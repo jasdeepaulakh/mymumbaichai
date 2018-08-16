37c123cbc4e8c042557108f5ec32cbb094b81ade
Jasdeep Aulakh <jasdeepaulakh@Jasdeeps-MacBook-Air.local>
2018-08-16 15:32:23 +0100

    Returns Product list

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
 class Products extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            products: [],
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://rest-test-jas.herokuapp.com/product')
+        .then(response =>{
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            let products = data.map((product) =>{
+                return(
+                    <div key={product.id}>
+                        <p>{product.name}</p>
+                    </div>
+                )
+            })
+    
+            
+        this.setState({products: products});
+        console.log("state", this.state.products);
+        })
+        
+    }
+
     render(){
         return(
+            <div>
             <p className="App-intro">
             Welcome to Products
             </p>
+
+                {this.state.products}
+            </div>
         )
     }
 }
